994e5e2a99964d8b76dc9f84a79a35a407f352c4
Malphabetagama <zamkdm786@gmail.com>
2020-12-30 12:09:52 +0530

    initialized firebase project

---
+++ .history/src/misc/firebase_20201230120629.jsx
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDZ8eT0TYVDvVHdVQhIve4Cq-ZxI7ArHj8",
+    authDomain: "chat-web-app-e5515.firebaseapp.com",
+    databaseURL: "https://chat-web-app-e5515-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-e5515",
+    storageBucket: "chat-web-app-e5515.appspot.com",
+    messagingSenderId: "35353350757",
+    appId: "1:35353350757:web:1d7db6b1a9555eebbaec8c"
+  };
